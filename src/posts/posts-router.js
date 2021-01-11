34c6a4a3f68d712c6c638c40d5099ac384a9be31
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-11 09:08:07 -0600

    made changes to post

--- src/posts/posts-router.js
+++ src/posts/posts-router.js
@@ -2,167 +2,116 @@ const express = require('express')
 const xss = require('xss')
 const path = require('path')
 const PostsService = require('./posts-service')
-
+const UsersService = require('../users/users-service')
 const postsRouter = express.Router()
 const jsonParser = express.json()
 
 const serializedPost = post =>({
-  post_id:post.post_id,
-  user_id:post.user_id,
-  title:xss(post.title),
-  link:xss(post.link),  
-  by:xss(post.by),
-  content:xss(post.content),
-  post_type:post.post_type,
-  username:xss(post.username),
-  date_created:post.date_created,
-  bookmark_content:xss(post.bookmark_content),
-  bookmark_id:post.bookmark_id,
-  image_path:xss(post.image_path)
+    id:post.id,
+    user_id:post.user_id,
+    title:xss(post.title),
+    link:xss(post.link),
+    start_date:post.start_date,
+    by:xss(post.by),
+    content:xss(post.content),
+    post_type:post.post_type,
+    date_created:post.date_created
 })
 
 postsRouter
-  .route('/')
-  .get((req, res, next)=>{
-     
-      const {userid, userbookmark}=req.query;
+  .route('/:username')
+  .get((req, res, next)=>{  
+  UsersService.getUserByUsername(
+    req.app.get('db'),
+    req.params.username
+  )
+  .then(user=>{
 
-      if(!userid && !userbookmark){
-          PostsService.getAllPosts(
-              req.app.get('db'),
-          )
-          .then(posts=>{
-              res.json(posts.map(serializedPost))
-          })
-          .catch(next)
+    if(!user){
+       return res.status(404).json({
+       error: {message: `User doesn't exist` }
+        })
       }
-      if(userid){
-          PostsService.getPostsByUserId(
-              req.app.get('db'),
-              userid
-          )
-          .then(posts=>{
-              if(posts.length===0){
-                  return res.status(404).json({
-                      error: {message: `Posts with that username or id do not exsit`}
-                  })
-              }
-              console.log(posts);
-              res.json(posts.map(serializedPost)) 
-          })
-          .catch(next)
-      }
+    
+      PostsService.getPostsByUserId(
+          req.app.get('db'),
+          user.id
+      )
+      .then(posts=>{
+          if(posts.length===0){
+            return res.status(404).json({
+            error: {message: `Posts with that username or id do not exsit`}
+            })
+          }
+          res.json(posts.map(serializedPost)) 
+        })
+      .catch(next)
       
-      if(userbookmark){
-          PostsService.getBookmarkPosts(
-              req.app.get('db'),
-              userbookmark
-          )
-          .then(posts=>{
-              if(posts.length===0){
-                  return res.status(404).json({
-                      error: {message: `That username or id does not exist`}
-                  })
-              }
-              res.json(posts) 
-          })
-          .catch(next)
-      }
   })
-  .post(jsonParser, (req, res, next)=>{
-      const { user_id, title, link, by,content, post_type, image_path } = req.body
-      const newPost = { user_id, title, link, by,content, post_type, image_path }
+  .catch(next)    
+ 
+    })
+    .post(jsonParser, (req, res, next)=>{
+       const { title, link, by,content, post_type } = req.body
+       const validPostTypes = [`recipe`, `lifestyle`,`event`,`book`,`podcast`]
 
-      const validPostTypes = [`recipe`, `lifestyle`,`event`,`book`,`podcast`]
-  
-          if(!user_id){
-              return res.status(400).json({
-                  error: { message : `Missing user_id in request body` }
-              })
+       UsersService.getUserByUsername(
+        req.app.get('db'),
+        req.params.username
+      )
+      .then(user=>{
+    
+        if(!user){
+           return res.status(404).json({
+           error: {message: `User doesn't exist` }
+            })
           }
-          if(!post_type){
-              return res.status(400).json({
-                  error: { message : `Missing post_type in request body` }
-              })
-          }
-
-          if(validPostTypes.indexOf(post_type)===-1){
-              return res.status(400).json({
-                  error: { message : `Post type must be either recipe, lifestyle, event ,book, or podcast` }
-              })
-          }
-
-          //check for required content depending on post_type
-          if(!image_path){
-              if(post_type==='event'&&(!title || !content || !link)){
-                  return res.status(400).json({
-                      error: { message : `Event post type must be include a title, content and link` }
-                  })
-              }
-              if(post_type==='lifestyle'&&(!title || !by || !link)){
-                  return res.status(400).json({
-                      error: { message : `Lifestyle post type must be include a title, by and link` }
-                  })
-              }
-              if(post_type==='podcast'&&(!title || !content || !link)){
-                  return res.status(400).json({
-                      error: { message : `Podcast post type must be include a title, content and link` }
-                  })
-              }
-              if(post_type==='book'&&(!title || !by )){
-                  return res.status(400).json({
-                      error: { message : `Book post type must be include a title and by` }
-                  })
-              }
-              if(post_type==='recipe' && !content){
-                  return res.status(400).json({
-                      error: { message : `Recipe post type must be include a content` }
-                  })
-              }
-          }
-
+          const user_id = user.id;
+          const newPost = { user_id, title, link,by,content, post_type }
           PostsService.insertNewPost(
-              req.app.get('db'),
+            req.app.get('db'),
               newPost
           )
           .then(post=>{
-              console.log(post)
-              res
-                  .status(201)
-                  .location(path.posix.join(req.originalUrl + `/${post.id}`))
-                  .json(serializedPost(post))
-          })
-          .catch(next) 
+             res
+          .status(201)
+             .location(path.posix.join(req.originalUrl + `/${post.id}`))
+             .json(serializedPost(post))
+            })            
+
+    })
+
   })
 postsRouter
-  .route(`/:post_id`)
-  .all((req, res, next)=>{
-      PostsService.getPostByPostId(
+    .route(`/:post_id`)
+    .all((req, res, next)=>{
+        PostsService.getPostByPostId(
           req.app.get('db'),
           req.params.post_id
-      )
-      .then(post=>{
-          if(!post){
-              return res.status(404).json({
-                  error: {message: `Post doesn't exist` }
-              })
-          }
-          res.post = post
-          next()
+        )
+    .then(post=>{
+       if(!post){
+         return res.status(404).json({
+         error: {message: `Post doesn't exist` }
+         })
+       }
+       res.post = post
+       next()
       })
-      .catch(next)       
-  })
-  .get((req, res, next)=>{
-      res.json(serializedPost(res.post))
-  })
-  .delete((req, res, next)=>{
-      PostsService.deletePost(
-          req.app.get('db'),
-          req.params.post_id
-      )
-      .then(()=>{
+    .catch(next)       
+    })
+    .get((req, res, next)=>{
+        res.json(serializedPost(res.post))
+    })
+    .delete((req, res, next)=>{
+       PostsService.deletePost(
+         req.app.get('db'),
+         req.params.post_id
+       )
+       .then(()=>{
           res.status(204).end()
-      })
-      .catch(next)
-  })
-module.exports = postsRouter
+        })
+        .catch(next)
+    })
+
+module.exports = postsRouter 
