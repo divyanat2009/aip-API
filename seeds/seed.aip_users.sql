c2747a5676ad725aa99bcc9b0767fcc029e7a9e6
divyanat2009 <69719463+divyanat2009@users.noreply.github.com>
2021-01-20 14:21:46 -0600

    changes to  seeds

--- seeds/seed.aip_users.sql
+++ seeds/seed.aip_users.sql
@@ -1,6 +1,8 @@
-INSERT INTO aip_users(user_id,fullname,username,password,date_created)
+INSERT INTO aip_users(fullname,username,password,date_created)
 VALUES
-('1','Divya Natarajan', 'divyanat','Genericpass123!',now() - '0 days'::INTERVAL),
-('2','Daenerys Targaryen', 'dtarg','Genericpass123!',now() - '0 days'::INTERVAL),
-('3','Uhtred Ragnarsson', 'uragnar', 'Genericpass123!',now() - '0 days'::INTERVAL)
+('Divya Natarajan', 'divyanat','Genericpass123!',now() - '0 days'::INTERVAL),
+('Danerys Targerean', 'dtarg','Genericpass123!',now() - '0 days'::INTERVAL),
+('Utred Ragnarson', 'uragnar', 'Genericpass123!',now() - '0 days'::INTERVAL),
+('Billy Strings', 'bstrings', 'needBetterPass2@',now() - '0 days'::INTERVAL),
+('Eddie Roberts', 'funkguitar', 'imissmusic4!',now() - '0 days'::INTERVAL)
 ;
